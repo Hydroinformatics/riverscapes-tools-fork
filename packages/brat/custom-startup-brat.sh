5ef001e51084d0ae6e80f9a22ad8e147871f069d
Evan Hackstadt <evan.hackstadt@gmail.com>
2025-07-25 11:38:41 -0700

    tweak startup "script"

--- packages/brat/custom-startup-brat.sh
+++ packages/brat/custom-startup-brat.sh
@@ -5,12 +5,12 @@
 # Script executed from: /packages/brat
 
 # We need these environment variables to be set:
-# $RS = path to the RS data directory
-# $HYDRO = path to the HYDRO data directory
 # $ANTHRO = path to the ANTHRO data directory
+# $HYDRO = path to the HYDRO data directory
+# $RS = path to the RS data directory
 # $VBET = path to the VBET data directory
 # $HUC10 = HUC10 code for the watershed to run BRAT on
-# $OUT_DIR = path to the output directory where BRAT results will be saved
+# $OUTPUT_DIR = path to the output directory where BRAT results will be saved
 
 '''TODO: write this into a proper script with permissions etc.
             Currently functioning as a place to stash commands to run manually.'''
@@ -21,9 +21,9 @@ cd /Users/evan/Code/OSU-EB3-REU/sqlBRAT/riverscapes-tools-fork/packages/brat
             so that this script can be generalized.'''
 
 echo "———Inputs Paths:———"
-echo "RS: $RS"
-echo "HYDRO: $HYDRO"
 echo "ANTHRO: $ANTHRO"
+echo "HYDRO: $HYDRO"
+echo "RS: $RS"
 echo "VBET: $VBET"
 echo "———Other Vars:———"
 echo "HUC10: $HUC10"
@@ -47,20 +47,72 @@ echo "Running BRAT for HUC $HUC10"
 brat $HUC10 \
     $RS/topography/dem_hillshade.tif \
     $HYDRO/outputs/hydro.gpkg/vwReaches \
-    $HYDRO/outputs/hydro.gpkg/IGOGeometry \
-    $HYDRO/outputs/hydro.gpkg/vwDgos \
+    $HYDRO/outputs/hydro.gpkg/vwIGOs \
+    $HYDRO/outputs/hydro.gpkg/vwDGOs \
     $ANTHRO/outputs/anthro.gpkg/vwReaches \
-    $ANTHRO/outputs/anthro.gpkg/IGOGeometry \
+    $ANTHRO/outputs/anthro.gpkg/vwIgos \
     $ANTHRO/outputs/anthro.gpkg/vwDgos \
     $RS/vegetation/existing_veg.tif \
     $RS/vegetation/historic_veg.tif \
     $VBET/outputs/vbet.gpkg/vbet_full \
-    30 \
-    100 \
-    $OUT_DIR \
+    30.0 \
+    100.0 \
+    $OUTPUT_DIR \
     --reach_codes 33400,33600,33601,33603,46000,46003,46006,46007 \
     --canal_codes 33600,33601,33603 \
     --peren_codes 46006 \
     --flow_areas $RS/hydrology/nhdplushr.gpkg/NHDArea \
     --waterbodies $RS/hydrology/nhdplushr.gpkg/NHDWaterbody \
+    --max_waterbody 0.001 \
     --verbose
+
+'''
+usage: brat [-h] [--reach_codes REACH_CODES] [--canal_codes CANAL_CODES]
+            [--peren_codes PEREN_CODES] [--flow_areas FLOW_AREAS]
+            [--waterbodies WATERBODIES] [--max_waterbody MAX_WATERBODY] [--meta META]
+            [--verbose] [--debug]
+            huc hillshade hydro_flowlines hydro_igos hydro_dgos anthro_flowlines
+            anthro_igos anthro_dgos existing_veg historical_veg valley_bottom
+            streamside_buffer riparian_buffer output_folder
+
+Build the inputs for an eventual brat_run:
+
+positional arguments:
+  huc                   huc input
+  hillshade             hillshade input
+  hydro_flowlines       hydro flowlines input
+  hydro_igos            hydro igos input
+  hydro_dgos            hydro dgos input
+  anthro_flowlines      anthro flowlines input
+  anthro_igos           anthro igos input
+  anthro_dgos           anthro dgos input
+  existing_veg          existing_veg input
+  historical_veg        historical_veg input
+  valley_bottom         Valley bottom shapeFile
+  streamside_buffer     streamside_buffer input
+  riparian_buffer       riparian_buffer input
+  output_folder         output_folder input
+
+options:
+  -h, --help            show this help message and exit
+  --reach_codes REACH_CODES
+                        Comma delimited reach codes (FCode) to retain when filtering
+                        features. Omitting this option retains all features.
+  --canal_codes CANAL_CODES
+                        Comma delimited reach codes (FCode) representing canals. Omitting
+                        this option retains all features.
+  --peren_codes PEREN_CODES
+                        Comma delimited reach codes (FCode) representing perennial features
+  --flow_areas FLOW_AREAS
+                        (optional) path to the flow area polygon feature class containing
+                        artificial paths
+  --waterbodies WATERBODIES
+                        (optional) waterbodies input
+  --max_waterbody MAX_WATERBODY
+                        (optional) maximum size of small waterbody artificial flows to be
+                        retained
+  --meta META           riverscapes project metadata as comma separated key=value pairs
+  --verbose             (optional) a little extra logging
+  --debug               (optional) more output about things like memory usage. There is a
+                        performance cost
+'''
